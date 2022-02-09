2117e249afc15faa4004bc2391cf2bb7d981c699
deandimarzo <deandimarzo@gmail.com>
2022-02-09 12:11:00 -0500

    Credits

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -236,7 +236,7 @@ class MainMenuState extends MusicBeatState
 					selectedSomethin = true;
 					FlxG.sound.play(Paths.sound('confirmMenu'));
 
-					if(ClientPrefs.flashing) FlxFlicker.flicker(magenta, 1.1, 0.15, false);
+					// if(ClientPrefs.flashing) FlxFlicker.flicker(magenta, 1.1, 0.15, false);
 
 					menuItems.forEach(function(spr:FlxSprite)
 					{
