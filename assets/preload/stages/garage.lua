d525a741e5fc34e7ce6b59005213bc1c557d5a2d
deandimarzo <deandimarzo@gmail.com>
2022-04-04 20:56:20 -0400

    New stuffff

---
+++ assets/preload/stages/garage.lua
@@ -0,0 +1,12 @@
+function onCreate()
+	-- background shit
+
+	makeLuaSprite('sunday-garage', 'sunday-garage', -600, -400);
+	setScrollFactor('sunday-garage', 0.9, 0.9);
+    scaleObject('sunday-garage', 1.2, 1.2);
+
+	addLuaSprite('sunday-garage', false);
+
+
+	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end
