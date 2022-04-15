4e719b1108023782831c2ecd2655e580ffcbbfa4
BCT2006 <bctix2006@gmail.com>
2022-04-15 10:35:51 -0700

    chromatic abberation on daw wars

--- assets/preload/data/daw-wars/windowshake.lua
+++ assets/preload/data/daw-wars/windowshake.lua
@@ -7,4 +7,22 @@ function opponentNoteHit(id, direction, noteType, isSustainNote)
 	setPropertyFromClass('lime.app.Application', 'current.window.y', getPropertyFromClass('lime.app.Application', 'current.window.y')-20)
 	setPropertyFromClass('lime.app.Application', 'current.window.x', getPropertyFromClass('lime.app.Application', 'current.window.x')+20)
 	setPropertyFromClass('lime.app.Application', 'current.window.y', getPropertyFromClass('lime.app.Application', 'current.window.y')+20)
+
+	
+	if isSustainNote then
+	runTimer('clearShader', 0.1)
+	else
+	addChromaticAbberationEffect('hud', 0.0005);
+	addChromaticAbberationEffect('game', 0.0005);
+	runTimer('clearShader', 0.1)
+	end
+end
+
+
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == 'clearShader' then
+		clearEffects('hud')
+		clearEffects('game')
+	end
 end
